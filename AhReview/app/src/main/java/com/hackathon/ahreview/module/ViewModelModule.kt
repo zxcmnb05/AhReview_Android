8125b66a0de50b405d9dd9388a2f4d2a6ce0e31c
임재현 <ljh040513@naver.com>
2021-11-24 21:42:14 +0900

    [ADD] findstore xml add
    ADD - 가게 찾기 xml add

--- AhReview/app/src/main/java/com/hackathon/ahreview/module/ViewModelModule.kt
+++ AhReview/app/src/main/java/com/hackathon/ahreview/module/ViewModelModule.kt
@@ -1,5 +1,6 @@
 package com.hackathon.ahreview.module
 
+import com.hackathon.ahreview.ui.findStore.FindStoreViewModel
 import com.hackathon.ahreview.ui.login.LoginViewModel
 import com.hackathon.ahreview.ui.main.MainViewModel
 import org.koin.androidx.viewmodel.dsl.viewModel
@@ -8,4 +9,6 @@ import org.koin.dsl.module
 val ViewModelModule = module {
     viewModel { MainViewModel() }
     viewModel { LoginViewModel() }
+
+    viewModel { FindStoreViewModel() }
 }
--- AhReview/app/src/main/java/com/hackathon/ahreview/ui/base/BaseViewModel.kt
+++ AhReview/app/src/main/java/com/hackathon/ahreview/ui/base/BaseViewModel.kt
@@ -1,6 +1,35 @@
 package com.hackathon.ahreview.ui.base
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import io.reactivex.Completable
+import io.reactivex.Single
+import io.reactivex.SingleObserver
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.disposables.Disposable
+import io.reactivex.observers.DisposableCompletableObserver
+import io.reactivex.observers.DisposableSingleObserver
+import io.reactivex.schedulers.Schedulers
 
 open class BaseViewModel: ViewModel() {
+    private val disposable = CompositeDisposable()
+
+    fun addDisposable(single: Single<*>, observer: DisposableSingleObserver<*>) {
+        disposable.add(single.subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribeWith(observer as SingleObserver<Any>) as Disposable
+        )
+    }
+
+    fun addDisposable(completable: Completable, observer: DisposableCompletableObserver) {
+        disposable.add(completable.subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread()).subscribeWith(observer))
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        disposable.clear()
+    }
 }
