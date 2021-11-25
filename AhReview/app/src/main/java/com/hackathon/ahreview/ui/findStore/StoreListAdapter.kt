701b6186b1b095b1de9d3bb79d468bdd98663a1c
임재현 <ljh040513@naver.com>
2021-11-26 00:38:58 +0900

    [FIX] store code fix

--- AhReview/app/src/main/java/com/hackathon/ahreview/ui/findStore/StoreListAdapter.kt
+++ AhReview/app/src/main/java/com/hackathon/ahreview/ui/findStore/StoreListAdapter.kt
@@ -1,6 +1,7 @@
 package com.hackathon.ahreview.ui.findStore
 
 import android.content.Context
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
@@ -9,6 +10,7 @@ import com.bumptech.glide.Glide
 import com.hackathon.ahreview.R
 import com.hackathon.ahreview.data.model.response.Store
 import com.hackathon.ahreview.databinding.StoreItemBinding
+import com.hackathon.ahreview.ui.detailStore.DetailStoreActivity
 
 class StoreListAdapter : RecyclerView.Adapter<StoreListAdapter.ViewHolder>() {
     lateinit var binding: StoreItemBinding
@@ -42,6 +44,17 @@ class StoreListAdapter : RecyclerView.Adapter<StoreListAdapter.ViewHolder>() {
             Glide.with(context)
                 .load(store.url)
                 .into(binding.storeImage)
+
+            binding.storeItemView.setOnClickListener {
+                val intent = Intent(context.applicationContext, DetailStoreActivity::class.java)
+                intent.putExtra("storeName", store.name)
+                intent.putExtra("address", store.address)
+                intent.putExtra("ammount", store.reviewAmount)
+                intent.putExtra("score", store.starScore)
+                intent.putExtra("url", store.url)
+
+                context.startActivity(intent)
+            }
         }
     }
 
